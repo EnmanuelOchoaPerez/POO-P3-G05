98fb90f4222697df144abfbe86feac7af2fc0189
EnmanuelOchoaPerez <149899527+EnmanuelOchoaPerez@users.noreply.github.com>
2025-06-28 14:45:09 -0500

    La base para el proyecto
    estas son las clases del modelo, no hay ninguna implementada aun, es nomas para tener una estructura

---
+++ POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Modelo/Servicio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poop3g05.poo.p3.g05.Modelo;
+
+/**
+ *
+ * @author Enmanuel
+ */
+public class Servicio {
+    
+}
---
+++ POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Vista/Menu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poop3g05.poo.p3.g05.Vista;
+
+/**
+ *
+ * @author Enmanuel
+ */
+public class Menu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

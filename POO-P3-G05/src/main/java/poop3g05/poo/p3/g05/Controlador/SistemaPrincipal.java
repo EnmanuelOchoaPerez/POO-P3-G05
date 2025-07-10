774bbad4b1d3036b59f5ac3f64d145e788bf3d07
EnmanuelOchoaPerez <149899527+EnmanuelOchoaPerez@users.noreply.github.com>
2025-07-09 22:24:51 -0500

    Subiendo 2 controladores
    "GestorTecnico" y "SistemaPrincipal" para revisar

--- POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Controlador/SistemaPrincipal.java
+++ POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Controlador/SistemaPrincipal.java
@@ -8,6 +8,7 @@ package poop3g05.poo.p3.g05.Controlador;
  *
  * @author Rafael Cosmo
  */
+//Para este avance no estan implementadas la mayoria de funcionalidades de la aplicacion. Gracias por la comprension
 import poop3g05.poo.p3.g05.Modelo.*;
 
 import poop3g05.poo.p3.g05.Vista.Menu;
@@ -15,34 +16,37 @@ import poop3g05.poo.p3.g05.Vista.Menu;
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
-        GestorClientes gestorClientes = new GestorClientes();
-        GestorServicios gestorServicios = new GestorServicios();
-        GestorTecnicos gestorTecnicos = new GestorTecnicos();
-        GestorOrdenes gestorOrdenes = new GestorOrdenes();
-        GestorFacturas gestorFacturas = new GestorFacturas();
+        GestorCliente gestorClientes = new GestorCliente();
+        GestorServicio gestorServicios = new GestorServicio();
+        GestorTecnico gestorTecnicos = new GestorTecnico();
+        GestorOrden gestorOrdenes = new GestorOrden();
+        GestorFactura gestorFacturas = new GestorFactura();
+        GestorProveedor gestorProveedor = new GestorProveedor();
 
         // Datos de prueba
-        inicializarApp(gestorClientes, gestorServicios, gestorTecnicos);
+        inicializarApp(gestorClientes, gestorServicios, gestorTecnicos,gestorProveedor);
 
         Menu.mostrarMenuPrincipal(
                 gestorClientes,
                 gestorServicios,
                 gestorTecnicos,
                 gestorOrdenes,
-                gestorFacturas
+                gestorFacturas,
+                gestorProveedor
         );
     }
 
     private static void inicializarApp(
-            GestorClientes gc,
-            GestorServicios gs,
-            GestorTecnicos gt
+            GestorCliente gc,
+            GestorServicio gs,
+            GestorTecnico gt,
+            GestorProveedor gp
     ) {
         // Agregar clientes
-        gc.agregarCliente(new Cliente("Juan Pérez", "0994871623", TipoCliente.PERSONAL));
-        gc.agregarCliente(new Cliente("Empresa XYZ", "0912748690", TipoCliente.EMPRESA));
-        gc.agregarCliente(new Cliente("María Gómez", "0931820475", TipoCliente.PERSONAL));
-        gc.agregarCliente(new Cliente("Constructora Delta", "0975086214", TipoCliente.EMPRESA));
+        gc.agregarUsuario(new Cliente("0824937125001", "0994871623", "Juan Pérez", "direccion", TipoCliente.PERSONAL));
+        gc.agregarUsuario(new Cliente("0637592816001", "0912748690", "Empresa XYZ", "direccion", TipoCliente.EMPRESA));
+        gc.agregarUsuario(new Cliente("0741068357", "0931820475", "María Gómez", "direccion", TipoCliente.PERSONAL));
+        gc.agregarUsuario(new Cliente("0519283742001", "0975086214", "Constructora Delta", "direccion", TipoCliente.EMPRESA));
 
         // Agregar servicios
         gs.agregarServicio(new Servicio("Cambio de aceite", 50.0));
@@ -53,14 +57,10 @@ public class SistemaPrincipal {
         gs.agregarServicio(new Servicio("Diagnóstico electrónico", 60.0));
 
         // Agregar técnicos
-        gt.agregarTecnico(new Tecnico("0967812049", "Carlos López", "Mecánico general"));
-        gt.agregarTecnico(new Tecnico("0924031687", "Andrea Ramírez", "Especialista en suspensión"));
-        
-//        new Proveedor("Filtros y Accesorios", "0954728136", "AutoFrenos Quito");
-//        new Proveedor("Distribuidora Eléctrica", "0973461820", "TecnoVolt Guayaquil");
+        gt.agregarUsuario(new Tecnico("0458926173", "0967812049", "Carlos López", "Mecánico general"));
+        gt.agregarUsuario(new Tecnico("0367819402", "0924031687", "Andrea Ramírez", "Especialista en suspensión"));
 
+        gp.agregarUsuario(new Proveedor("0741067125001", "0954728136","Filtros y Accesorios", "AutoFrenos Quito"));
+        gp.agregarUsuario(new Proveedor("0824992816001", "0973461820","Distribuidora Eléctrica", "TecnoVolt Guayaquil"));
     }
 }
-/*
-
-*/

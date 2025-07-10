4b73309f1659c0eb91c2a570605752b681c2157a
EnmanuelOchoaPerez <149899527+EnmanuelOchoaPerez@users.noreply.github.com>
2025-07-09 22:06:07 -0500

    Primer controlador "GestorCliente" para revisar

--- POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Controlador/GestorCliente.java
+++ POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Controlador/GestorCliente.java
@@ -3,35 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package poop3g05.poo.p3.g05.Controlador;
+import poop3g05.poo.p3.g05.Modelo.Cliente;
 
 /**
  *
  * @author Rafael Cosmo
  */
+import poop3g05.poo.p3.g05.Modelo.TipoCliente;
 
-import poop3g05.poo.p3.g05.Modelo.Cliente;
-import java.util.ArrayList;
-import java.util.List;
+public class GestorCliente extends GestorUsuario<Cliente> {
 
-public class GestorCliente {
-    private List<Cliente> clientes = new ArrayList<>();
-    private int nextId = 1;
-
-    public void agregarCliente(Cliente cliente) {
-        cliente.setId(nextId++);
-        clientes.add(cliente);
+    public void editarTipoCliente(String id, TipoCliente tipo) {
+        ((Cliente) buscarUsuario(id)).setTipoCliente(tipo);
     }
 
-    public Cliente buscarCliente(int id) {
-        for (Cliente cliente : clientes) {
-            if (cliente.getId() == id) {
-                return cliente;
-            }
-        }
-        return null;
-    }
-
-    public List<Cliente> getClientes() {
-        return new ArrayList<>(clientes);
+    public void editarDireccionCliente(String id, String direccioNueva) {
+        ((Cliente) buscarUsuario(id)).setDireccion(direccioNueva);
     }
 }

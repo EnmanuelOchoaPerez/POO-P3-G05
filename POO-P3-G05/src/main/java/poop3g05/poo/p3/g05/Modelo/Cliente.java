afb528a7ae5eb9625ccd353807de5d3ed1b878bc
EnmanuelOchoaPerez <149899527+EnmanuelOchoaPerez@users.noreply.github.com>
2025-07-09 22:34:13 -0500

    Actualizando Modelo

--- POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Modelo/Cliente.java
+++ POO-P3-G05/src/main/java/poop3g05/poo/p3/g05/Modelo/Cliente.java
@@ -8,22 +8,41 @@ package poop3g05.poo.p3.g05.Modelo;
  *
  * @author Rafael Cosmo
  */
-
 public class Cliente extends Usuario {
 
     private TipoCliente tipoCliente;
+    private String direccion;
 
-    public Cliente(String nombre, String telefono, TipoCliente tipoCliente) {
-        super(telefono, nombre);
+    public Cliente(String id, String telefono, String nombre, String direccion, TipoCliente tipoCliente) {
+        super(id, telefono, nombre);
         this.tipoCliente = tipoCliente;
+        this.direccion = direccion;
     }
 
     // Getters y Setters
-    public TipoCliente getTipoCliente() { return tipoCliente; }
-    public void setTipoCliente(TipoCliente tipoCliente) { this.tipoCliente = tipoCliente; }
-    
+    public TipoCliente getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
     @Override
     public String toString() {
-        return username + " (" + tipoCliente + ")";
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente : ");
+        sb.append(super.toString());
+        sb.append(", ").append(direccion);
+        sb.append(", ").append(tipoCliente);
+        return sb.toString();
     }
 }
